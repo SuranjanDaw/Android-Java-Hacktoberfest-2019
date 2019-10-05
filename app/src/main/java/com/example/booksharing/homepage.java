ac10bb8928f2290afbe487968f1b93947b5aece1
Shreeghanesh Balaji <>
2019-10-05 13:41:15 +0530

    [Retrofit Integration] - Integrates Retrofit and call JSONPlaceholder api to populate result

---
+++ app/src/main/java/com/example/booksharing/homepage.java
@@ -0,0 +1,89 @@
+package com.example.booksharing;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Homepage extends AppCompatActivity {
+    GoogleSignInClient mGoogleSignInClient;
+    FirebaseAuth fbauth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(Homepage.this);
+        if (acct != null) {
+            String personName = acct.getDisplayName();
+            String personGivenName = acct.getGivenName();
+            String personFamilyName = acct.getFamilyName();
+            String personEmail = acct.getEmail();
+            String personId = acct.getId();
+            Uri personPhoto = acct.getPhotoUrl();
+        }
+
+        fbauth = FirebaseAuth.getInstance();
+        BottomNavigationView navigationView = findViewById(R.id.btm_nav);
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                int id = menuItem.getItemId();
+
+                if (id == R.id.apiCaller) {
+                    HomeFragment fragment = new HomeFragment();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.frame_layout, fragment);
+                    fragmentTransaction.commit();
+
+                }
+                if (id == R.id.qr_scanner) {
+                    QR_ScanFragment fragment = new QR_ScanFragment();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.frame_layout, fragment);
+                    fragmentTransaction.commit();
+                }
+                return true;
+            }
+        });
+        //The default fragment is home(apiCaller)
+        navigationView.setSelectedItemId(R.id.apiCaller);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logoutmenu: {
+                fbauth.signOut();
+                finish();
+                startActivity(new Intent(Homepage.this, MainActivity.class));
+
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
